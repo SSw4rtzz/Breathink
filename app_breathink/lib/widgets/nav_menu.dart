f1f5f68db6e9123d008a5b102889d65e25e1cdc3
Ruben <4aronswartz@gmail.com>
2024-11-21 20:44:40 +0000

    Icons + Funcional de testes
    Adicionado à barra de navegação icons, marcador de selecionado e mudança de screen ficticia, para fins de testes

---
+++ app_breathink/lib/widgets/nav_menu.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:get/get.dart';
+
+class NavMenu extends StatelessWidget {
+  const NavMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) => controller.selectedIndex.value = index,
+          destinations: const [
+            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Iconsax.book), label: 'Books'),
+            NavigationDestination(icon: Icon(Iconsax.menu), label: 'Stacks'),
+            NavigationDestination(icon: Icon(Iconsax.profile), label: 'Profile'),
+          ],
+        ),
+      ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
+
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [Container(color: Colors.red), Container(color: Colors.green), Container(color: Colors.blue), Container(color: Colors.yellow)
+  ];
+}
