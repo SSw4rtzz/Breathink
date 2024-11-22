bba783dfa2ab5f52792faaf1a5c1d186b363a318
Ruben <4aronswartz@gmail.com>
2024-11-22 00:59:55 +0000

    Navbar arredondada (Bug visual)

--- app_breathink/lib/widgets/nav_menu.dart
+++ app_breathink/lib/widgets/nav_menu.dart
@@ -11,17 +11,54 @@ class NavMenu extends StatelessWidget {
 
     return Scaffold(
       bottomNavigationBar: Obx(
-        () => NavigationBar(
-          height: 80,
-          elevation: 0,
-          selectedIndex: controller.selectedIndex.value,
-          onDestinationSelected: (index) => controller.selectedIndex.value = index,
-          destinations: const [
-            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
-            NavigationDestination(icon: Icon(Iconsax.book_1), label: 'Livros'),
-            NavigationDestination(icon: Icon(Iconsax.note_2), label: 'Estante'),
-            NavigationDestination(icon: Icon(Iconsax.cup), label: 'Objetivos'),
-          ],
+        () => Padding(
+          padding: const EdgeInsets.all(16.0), // Espaçamento geral ao redor
+          child: Container(
+            margin: const EdgeInsets.only(bottom: 16), // Espaço abaixo (flutua acima do fundo)
+            height: 80,
+            decoration: BoxDecoration(
+              color: Colors.black, // Cor do fundo do menu
+              borderRadius: BorderRadius.circular(40), // Cantos arredondados
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2), // Sombra
+                  spreadRadius: 1,
+                  blurRadius: 10,
+                  offset: const Offset(0, 5), // Sombra deslocada para baixo
+                ),
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(40),
+              child: NavigationBar(
+                height: 80,
+                elevation: 0,
+                backgroundColor: Colors.transparent, // Remove a cor padrão
+                labelBehavior: NavigationDestinationLabelBehavior.alwaysShow, // Garante que o texto aparece
+                destinations: const [
+                  NavigationDestination(
+                    icon: Icon(Iconsax.home), 
+                    label: 'Home',
+                  ),
+                  NavigationDestination(
+                    icon: Icon(Iconsax.book_1), 
+                    label: 'Livros',
+                  ),
+                  NavigationDestination(
+                    icon: Icon(Iconsax.note_2), 
+                    label: 'Estante',
+                  ),
+                  NavigationDestination(
+                    icon: Icon(Iconsax.cup), 
+                    label: 'Objetivos',
+                  ),
+                ],
+                selectedIndex: controller.selectedIndex.value,
+                onDestinationSelected: (index) =>
+                    controller.selectedIndex.value = index,
+              ),
+            ),
+          ),
         ),
       ),
       body: Obx(() => controller.screens[controller.selectedIndex.value]),
@@ -29,10 +66,13 @@ class NavMenu extends StatelessWidget {
   }
 }
 
-
 class NavigationController extends GetxController {
   final Rx<int> selectedIndex = 0.obs;
 
-  final screens = [Container(color: Colors.red), Container(color: Colors.green), Container(color: Colors.blue), Container(color: Colors.yellow)
+  final screens = [
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+    Container(color: Colors.blue),
+    Container(color: Colors.yellow),
   ];
 }
